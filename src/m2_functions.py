f876934702f4e9a1dda949b82b5973ea255a2a29
bressmjm <bressmjm@rose-hulman.edu>
2018-09-03 15:25:28 -0400

    Project #2

--- src/m2_functions.py
+++ src/m2_functions.py
@@ -3,7 +3,7 @@ Practice DEFINING and CALLING
      FUNCTIONS
 
 Authors: David Mutchler, Dave Fisher, Vibha Alangar, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Joshua Bressman. 
 """  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
